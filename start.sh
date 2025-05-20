9b9ddfb768e26c3d5f88e2dcda5cfbe9da684f3d
Daniel Arcade <83617030+DanielArcade@users.noreply.github.com>
2025-05-19 22:32:48 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,32 +1,30 @@
 
 
-
-7z x  attractplus.7z 
-rm -r attractplus.7z
+7z x "/home/arcade/Update/attractplus.7z" -o"/home/arcade/Update"
+rm "/home/arcade/Update/attractplus.7z"
 sudo cp "/home/arcade/Update/attractplus" "/usr/local/bin"
 sudo chmod 777 "/usr/local/bin/attractplus"
 
  
- 
-sudo cp qr-code /usr/local/bin
+sudo cp -r "/home/arcade/Update/qr-code" "/usr/local/bin"
 
 sleep 2
 sudo chmod 777 /usr/bin/qr-code
 
 
-sudo cp "/home/arcade/Update/cobranca" "/home/arcade/shared/frontends/attract"
+sudo cp -r "/home/arcade/Update/cobranca" "/home/arcade/shared/frontends/attract"
 sleep 2
 sudo chmod -R 777 /home/arcade/shared/frontends/attract/cobranca
 
 
-sudo cp "/home/arcade/Update/teclado" "/usr/local/bin"
+sudo cp -r "/home/arcade/Update/teclado" "/usr/local/bin"
 
 sleep 2
 sudo chmod 777 /usr/bin/teclado 
 
 
-sudo rm -rf /home/arcade/Update
-sudo rm -rf /home/arcade/.local/share/Trash
+sudo rm -R /home/arcade/Update
+sudo rm -R /home/arcade/.local/share/Trash
 #sudo pacman -Sy --noconfirm
 #sudo chmod -R 777 /home/arcade/shared/frontends/attract
 dialog --infobox "Reiniciando..." 10 20 
